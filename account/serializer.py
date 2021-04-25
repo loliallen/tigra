9b2a7d18f2994a818f0b9457185687f38b032d70
loliallen <levaya.pochta.sobaka@gmail.com>
2021-04-25 20:55:46 +0300

    wasd

--- account/serializer.py
+++ account/serializer.py
@@ -24,7 +24,7 @@ class CreateUserSerializer(UserCreateSerializer):
 
     class Meta:
         model = User
-        fields = ("__all_", "my_invintations")
+        fields = ("__all__",)
 
     def create(self, validated_data):
         print(validated_data.__dict__)
