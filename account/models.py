6bb7fa62bae6d9a5324564aca06f985448b765b4
loliallen <levaya.pochta.sobaka@gmail.com>
2021-04-25 20:52:27 +0300

    add fcm

--- account/models.py
+++ account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models import signals
 from django.contrib.auth.models import AbstractUser
-
+import server.firebase as fcm
 from mobile.models import Visit
 
 from string import digits, ascii_uppercase
