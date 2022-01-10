53748f8799fe1e4018ca2b687b57f73b4d811dc7
intsynko <intsyn@mail.ru>
2022-01-10 22:30:09 +0300

    added tests on info and phone confirm

--- account/controller.py
+++ account/controller.py
@@ -7,7 +7,14 @@ SMS_AERO_API_KEY = os.environ.get("SMS_AERO_API_KEY")
 SMS_AERO_USERNAME = os.environ.get("SMS_AERO_USERNAME")
 SMS_AERO_URL = "https://email:api_key@gate.smsaero.ru/v2/sms/send"
 
+
 def sendCode(phone, code):
+    # for pathing in tests
+    _send_code(phone, code)
+
+
+def _send_code(phone, code):
+    print('sendCode called')
     custom_params = {
         "number": phone,
         "text":"Your one time code is: {}".format(code),
--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from rest_framework.test import APIClient
 
@@ -5,7 +7,7 @@ from account.models import User
 from account.test_utils import UserFactory
 
 
-class AccountTestCase(TestCase):
+class RegisterTest(TestCase):
     auth_data = {
         "password": "SupperPassword123",
         "phone": "phone",
@@ -15,9 +17,6 @@ class AccountTestCase(TestCase):
         "first_name": "",
         "last_name": "",
     }
-
-    def setUp(self):
-        self.client = APIClient()
 
     def test_register(self):
         # пытаемся создать юзера
@@ -38,23 +37,76 @@ class AccountTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('auth_token', response.json())
 
-    def test_set_device_token(self):
-        # обновляем токен устрйоства пользователю
+
+class AccountTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def get_auth_client(self):
+        client = APIClient()
         password = 'P@ssword'
         user = UserFactory(password=password)
-        response = self.client.post('/account/token/login', data={
+        response = client.post('/account/token/login', data={
             'phone': user.phone,
             'password': password,
         })
         self.assertEqual(response.status_code, 200)
         self.assertIn('auth_token', response.json())
         auth_token = response.json()['auth_token']
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {auth_token}')
+        client.credentials(HTTP_AUTHORIZATION=f'Token {auth_token}')
+        return client, user
 
+    def test_set_device_token(self):
+        # обновляем токен устрйоства пользователю
+        client, user = self.get_auth_client()
         self.assertEqual(user.device_token, '')
-        response = self.client.post('/account/device/', data={
+        response = client.post('/account/device/', data={
             'token': 'some new token'
         })
         user.refresh_from_db()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(user.device_token, 'some new token')
+
+    def test_info(self):
+        # проверяем наличие инфы о пользователе
+        client, user = self.get_auth_client()
+        response = client.get('/account/about/me/')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['visits'], [])
+        self.assertEqual(data['children'], [])
+        self.assertEqual(data['used_invintation'], None)
+        self.assertEqual(data['my_invintations'], [])
+        self.assertIn('phone', data)
+        self.assertIn('username', data)
+        self.assertEqual(data['phone_confirmed'], False)
+        self.assertEqual(data['device_token'], '')
+
+    @patch('account.controller._send_code')
+    def test_confirm_phone(self, send_code_mock):
+        # подтверждение телефона
+        client, user = self.get_auth_client()
+        response = client.get('/account/about/me/')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['phone_confirmed'], False)
+
+        response = self.client.post('/account/confirm/', data={
+            'phone': user.phone
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(send_code_mock.call_args.args[0], user.phone)
+        code = send_code_mock.call_args.args[1]
+
+        response = self.client.get('/account/confirm/', data={
+            'id': data['id'],
+            'code': '',
+        })
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.get('/account/confirm/', data={
+            'id': data['id'],
+            'code': code,
+        })
+        self.assertEqual(response.status_code, 200)
